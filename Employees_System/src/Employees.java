11d759277ee4c049f3039484e385e4a15c46ce55
Moamen Mekky <moamen.mekky@valeo.com>
2024-02-14 23:18:36 +0200

    added minue , addEmployee ,lstEmployees

--- Employees_System/src/Employees.java
+++ Employees_System/src/Employees.java
@@ -1,2 +1,30 @@
-public class Employees {
+public class Employees
+{
+    private String name;
+    private  int age ;
+    private  int salary ;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
 }
--- Employees_System/src/Main.java
+++ Employees_System/src/Main.java
@@ -1,8 +1,74 @@
+import jdk.dynalink.beans.StaticClass;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
+     static Scanner in= new Scanner(System.in);
+    static ArrayList <Employees> employeesList = new ArrayList<>(20);
     public static void main(String[] args)
     {
+        minu();
 
+
+
+
+
+
+    }
+    public static void addEmployee ()
+    {
+        String name;
+        int age ,salary;
+        Employees employee = new Employees();
+        System.out.println("enter employee name ");
+        name=in.nextLine();
+        employee.setName(name);
+        System.out.println("enter employee age ");
+        age=in.nextInt();
+        employee.setAge(age);
+        System.out.println("enter employee salary ");
+        salary=in.nextInt();
+        name=in.nextLine();
+        employee.setSalary(salary);
+        employeesList.add(employee);
+        minu();
+    }
+    static  void listEmployees()
+    {
+        int size= employeesList.size();
+        if(size<1)
+        {
+            System.out.println("no employess in the list!");
+            System.out.println();
+        }
+        else
+            for (Employees x:employeesList)
+            {
+                System.out.printf("Employee %s has age %d and salary %d \n",x.getName(),x.getAge(),x.getSalary());
+                System.out.println();
+
+
+            }
+            minu();
+
+    }
+    static void   minu()
+    {
+        System.out.println();
+        System.out.println("enter 1 for adding new Employee");
+        System.out.println("enter 2 for list all Employees ");
+        System.out.println("enter 3 for delete Employee");
+        System.out.println("enter 4 for update Employee salary ");
+        System.out.println("enter 5 to Exit ");
+        int choice =in.nextInt();
+        String x=in.nextLine();
+        if (choice==1)
+            addEmployee();
+        else if (choice==2) {
+            listEmployees();
+
+        }
 
     }
 }
